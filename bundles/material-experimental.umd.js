c65edb23d93ad02ba3bb9b2c6590025dfdb069f7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-12 21:38:46 +0000

    master - 0dbe23bb9 fix(menu): allow focus to be moved inside menuClosed event (#18756)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('9.1.2-sha-76c3fb6db');
+	var VERSION = new core.Version('9.1.2-sha-0dbe23bb9');
 
 	/**
 	 * @license
--- fesm2015/material-experimental.js
+++ fesm2015/material-experimental.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Material experimental package. */
-const VERSION = new Version('9.1.2-sha-76c3fb6db');
+const VERSION = new Version('9.1.2-sha-0dbe23bb9');
 
 /**
  * @license
