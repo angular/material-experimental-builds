a24594933935c6333c95c3e096c498d43d1235d7
mmalerba <mmalerba@google.com>
2020-03-19 17:38:22 +0000

    master - 734ca1540 chore: bump version to 9.2.0-next.0 w/ changelog (#18860)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('9.1.3-sha-26364fa72');
+	var VERSION = new core.Version('9.2.0-next.0-sha-734ca1540');
 
 	/**
 	 * @license
