5a23bdcb29e110190da4e7b0990e1d4b160f2210
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-21 05:18:30 +0000

    master - 6275339f0 fix(form-field): incorrectly calculating start gap in RTL in the presence of a prefix (#18867)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('9.2.0-next.0-sha-32aec936b');
+	var VERSION = new core.Version('9.2.0-next.0-sha-6275339f0');
 
 	/**
 	 * @license
