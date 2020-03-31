72fe63e8341b0e7f9293018257905c8bfa7d0182
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-31 16:11:26 +0000

    master - f70f49af7 fix(popover-edit): hover content not showing up if content changes after init (#18937)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('9.2.0-sha-a0695ca0d');
+	var VERSION = new core.Version('9.2.0-sha-f70f49af7');
 
 	/**
 	 * @license
