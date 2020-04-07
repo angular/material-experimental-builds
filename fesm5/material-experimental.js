9e6daf6c6ed7c97e7aed37b525331dbeca0e5046
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-04-07 19:16:11 +0000

    master - 3554e38ed refactor: remove unused sass variables in non-theme files (#18911)

--- fesm5/material-experimental.js
+++ fesm5/material-experimental.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the Material experimental package. */
-var VERSION = new Version('9.2.0-sha-b225b8757');
+var VERSION = new Version('9.2.0-sha-3554e38ed');
 
 /**
  * @license
