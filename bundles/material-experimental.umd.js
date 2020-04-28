c2bbc2229bf17e1b3409b00ce1966ea7384ab36b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-28 21:01:30 +0000

    master - afbaad326 build: add server-side rendering check for ground overlay (#19188)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('9.2.1-sha-76f1341e3');
+	var VERSION = new core.Version('9.2.1-sha-afbaad326');
 
 	/**
 	 * @license
