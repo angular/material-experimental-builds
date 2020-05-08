fb91b4f633a090d7ada17ddc4f0ba4f0c6fb4aac
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-08 19:54:15 +0000

    master - f8d9c96e5 fix(input): avoid repeat accesses to check if element is a textarea (#19115)

--- bundles/material-experimental-mdc-input.umd.js
+++ bundles/material-experimental-mdc-input.umd.js
@@ -227,7 +227,7 @@
                             '[class.mat-form-field-autofill-control]': 'false',
                             '[class.mat-input-element]': 'false',
                             '[class.mat-input-server]': '_isServer',
-                            '[class.mat-mdc-textarea-input]': '_isTextarea()',
+                            '[class.mat-mdc-textarea-input]': '_isTextarea',
                             // Native input properties that are overwritten by Angular inputs need to be synced with
                             // the native input element. Otherwise property bindings for those don't work.
                             '[id]': 'id',
