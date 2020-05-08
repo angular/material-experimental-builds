91fbf39add1762a4e07f995fd21c02e08420a360
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-08 19:54:05 +0000

    master - 7fde30524 fix(list): selection list marked as touched when tabbing in (#19177)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('9.2.1-sha-b38c812da');
+	var VERSION = new core.Version('9.2.1-sha-7fde30524');
 
 	/**
 	 * @license
