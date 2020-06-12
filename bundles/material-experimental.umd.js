b798c40b6b3c5be75e05f9d5dfbdff68d238bf43
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-12 17:21:58 +0000

    master - a9cabf593 build: remove jelbourn from top-level files as reviewer (#19598)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('10.0.0-rc.1-sha-6bf7f246f');
+	var VERSION = new core.Version('10.0.0-rc.1-sha-a9cabf593');
 
 	/**
 	 * @license
