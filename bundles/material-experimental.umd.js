4daa29e7bae44bb52335d80c9640aec666045ef9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-17 23:02:33 +0000

    master - 3e3e2c5f8 docs(datepicker): switch examples to non-deprecated form field appearance (#19653)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('10.0.0-rc.2-sha-cb625decb');
+	var VERSION = new core.Version('10.0.0-rc.2-sha-3e3e2c5f8');
 
 	/**
 	 * @license
