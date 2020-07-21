43d52aa5cd9d2e047bb03b3214442f69d31d26e3
Jeremy Elbourn <jelbourn@google.com>
2020-07-21 23:30:50 +0000

    master - bbce2de12 docs(readme): expand MDC point to mention benefits (#20059)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('10.1.0-sha-e16997cbe');
+	var VERSION = new core.Version('10.1.0-sha-bbce2de12');
 
 	/**
 	 * @license
