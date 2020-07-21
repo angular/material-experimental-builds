f00d0318887a86fb5ea917f3688d0b684951eaca
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-21 19:37:15 +0000

    master - b3099c839 fix(table): no data row not shown if data source is empty on init (#19994)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('10.1.0-sha-a3a8490c6');
+	var VERSION = new core.Version('10.1.0-sha-b3099c839');
 
 	/**
 	 * @license
