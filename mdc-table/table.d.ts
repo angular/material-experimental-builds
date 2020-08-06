4b943fe94558809c8e618e23c12ab79dcb89e2b1
Karl Seamon <bannanafiend@gmail.com>
2020-08-06 17:21:26 +0000

    master - 1b1c31e91 perf(table): Slightly improve speed of adding/remvoing sticky styles (#19823)

--- mdc-table/table.d.ts
+++ mdc-table/table.d.ts
@@ -10,5 +10,7 @@ import { CdkTable } from '@angular/cdk/table';
 export declare class MatTable<T> extends CdkTable<T> implements OnInit {
     /** Overrides the sticky CSS class set by the `CdkTable`. */
     protected stickyCssClass: string;
+    /** Overrides the need to add position: sticky on every sticky cell element in `CdkTable`. */
+    protected needsPositionStickyOnElement: boolean;
     ngOnInit(): void;
 }
