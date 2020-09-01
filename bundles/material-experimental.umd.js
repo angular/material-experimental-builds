e96f427be57fdec48ad19599a1d66916d4b19be4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-01 20:49:02 +0000

    master - 98d4799cb refactor(material): remove month constants from core (#18111)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.0.0-next.0-sha-3c2334cb3');
+	var VERSION = new core.Version('11.0.0-next.0-sha-98d4799cb');
 
 	/**
 	 * @license
