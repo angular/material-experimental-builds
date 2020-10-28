ea8b4f8a8a90a4f37bb0e04a71b5279a12e0ec70
Annie Wang <annieyw@google.com>
2020-10-28 03:11:31 +0000

    master - e2ebfe040 docs(material/autocomplete): add autocomplete harness example (#20830)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-ec252f835');
+	var VERSION = new core.Version('11.1.0-next.0-sha-e2ebfe040');
 
 	/**
 	 * @license
