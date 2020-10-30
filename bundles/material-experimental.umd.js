aac46e89db55e00174ab3026e6826326b428c793
mmalerba <mmalerba@google.com>
2020-10-30 03:55:32 +0000

    11.0.x - 6add7febb fix(material-experimental/mdc-snack-bar): add missing exports (#20928)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.0.0-rc.0-sha-828ece0bf');
+	var VERSION = new core.Version('11.0.0-rc.0-sha-6add7febb');
 
 	/**
 	 * @license
