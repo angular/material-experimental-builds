0ebed07174d5e2f0e7ffd5ee9576bf7f312c2707
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-07 00:06:06 +0000

    master - 92bb1c89d fix(material-experimental/mdc-button): narrow down overly-broad selector (#20969)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-d686290ff');
+	var VERSION = new core.Version('11.1.0-next.0-sha-92bb1c89d');
 
 	/**
 	 * @license
