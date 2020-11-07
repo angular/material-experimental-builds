5491c0f7ee2de3303fe3d97cebe899032c6388ea
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-07 00:09:19 +0000

    master - 6ec155157 fix(table): filter predicate not called for falsy values (#19094)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-b9756b998');
+	var VERSION = new core.Version('11.1.0-next.0-sha-6ec155157');
 
 	/**
 	 * @license
