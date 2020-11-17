787afdee981579da545f877fb83ef24e90ebe543
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-17 17:12:20 +0000

    master - efd8ab640 fix(material-experimental/mdc-progress-spinner): prevent animation from affecting surrounding layout (#20962)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-eda397c8d');
+	var VERSION = new core.Version('11.1.0-next.0-sha-efd8ab640');
 
 	/**
 	 * @license
