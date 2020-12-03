1db2d716093ab3b191e2fcb4f0a44581b2694349
Joep Kockelkorn <joepkockelkorn@gmail.com>
2020-12-03 18:49:19 +0000

    master - ab19ecca1 feat(table): expand cell harness api (#21162)

--- bundles/material-experimental-mdc-table-testing.umd.js
+++ bundles/material-experimental-mdc-table-testing.umd.js
@@ -356,7 +356,7 @@
             });
         };
         return MatCellHarness;
-    }(testing.ComponentHarness));
+    }(testing.ContentContainerComponentHarness));
     /** The selector for the host element of a `MatCellHarness` instance. */
     MatCellHarness.hostSelector = '.mat-mdc-cell';
     /** Harness for interacting with an MDC-based Angular Material table header cell. */
