c5940ef7d94ac11096cc5757d85aa44c52ae979a
mmalerba <mmalerba@google.com>
2020-12-12 00:07:17 +0000

    11.0.x - c6231e19b refactor(material/core): make typography normalization function private (#21286)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.0.2-sha-75e23cb8d');
+	var VERSION = new core.Version('11.0.2-sha-c6231e19b');
 
 	/**
 	 * @license
