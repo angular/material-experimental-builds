36822ec74828d9f85ebfb4bd25a3b0cf8b454d18
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-12-17 22:05:09 +0000

    master - 411b1747d fix(cdk/drag-drop): account for enterPredicate when setting receiving class (#21346)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-b8cdef45a');
+	var VERSION = new core.Version('11.1.0-next.0-sha-411b1747d');
 
 	/**
 	 * @license
