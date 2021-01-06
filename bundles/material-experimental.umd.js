20e133dbc7313804b82681547e700a6552596c71
Annie Wang <annieyw@google.com>
2021-01-06 12:01:34 +0000

    master - 1b4741212 docs(material/divider): add harness example for divider (#21473)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-2c73a4c19');
+	var VERSION = new core.Version('11.1.0-next.0-sha-1b4741212');
 
 	/**
 	 * @license
