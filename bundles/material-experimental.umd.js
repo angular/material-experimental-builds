79c717ddb89dc136e33cbdbb6c7b32be4d0bcba0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-07 13:46:03 +0000

    master - 841d9e1e2 build: bump year (#21486)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.1.0-next.0-sha-461602d4a');
+	var VERSION = new core.Version('11.1.0-next.0-sha-841d9e1e2');
 
 	/**
 	 * @license
