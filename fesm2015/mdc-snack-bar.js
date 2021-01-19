0fb9d6bd5b1ea1293702754d83433b654e1f583f
Annie Wang <annieyw@google.com>
2021-01-19 17:25:03 +0000

    11.1.x - f76369ab7 fix(material/snack-bar): fix Firefox/JAWS not reading out snackbar message (#21552)

--- fesm2015/mdc-snack-bar.js
+++ fesm2015/mdc-snack-bar.js
@@ -110,6 +110,16 @@ class MatSnackBarContainer extends BasePortalOutlet {
         else {
             this._live = 'polite';
         }
+        // Only set role for Firefox. Set role based on aria-live because setting role="alert" implies
+        // aria-live="assertive" which may cause issues if aria-live is set to "polite" above.
+        if (this._platform.FIREFOX) {
+            if (this._live === 'polite') {
+                this._role = 'status';
+            }
+            if (this._live === 'assertive') {
+                this._role = 'alert';
+            }
+        }
         // `MatSnackBar` will use the config's timeout to determine when the snack bar should be closed.
         // Set this to `-1` to mark it as indefinitely open so that MDC does not close itself.
         this._mdcFoundation.setTimeoutMs(-1);
@@ -211,7 +221,7 @@ class MatSnackBarContainer extends BasePortalOutlet {
 MatSnackBarContainer.decorators = [
     { type: Component, args: [{
                 selector: 'mat-mdc-snack-bar-container',
-                template: "<div class=\"mdc-snackbar__surface\" #surface>\n  <!--\n    This outer label wrapper will have the class `mdc-snackbar__label` applied if\n    the attached template/component does not contain it.\n  -->\n  <div class=\"mat-mdc-snack-bar-label\" #label>\n    <!-- Initialy holds the snack bar content, will be empty after announcing to screen readers. -->\n    <div aria-hidden=\"true\">\n      <ng-template cdkPortalOutlet></ng-template>\n    </div>\n\n    <!-- Will receive the snack bar content from the non-live div, move will happen a short delay after opening -->\n    <div [attr.aria-live]=\"_live\"></div>\n  </div>\n</div>\n",
+                template: "<div class=\"mdc-snackbar__surface\" #surface>\n  <!--\n    This outer label wrapper will have the class `mdc-snackbar__label` applied if\n    the attached template/component does not contain it.\n  -->\n  <div class=\"mat-mdc-snack-bar-label\" #label>\n    <!-- Initialy holds the snack bar content, will be empty after announcing to screen readers. -->\n    <div aria-hidden=\"true\">\n      <ng-template cdkPortalOutlet></ng-template>\n    </div>\n\n    <!-- Will receive the snack bar content from the non-live div, move will happen a short delay after opening -->\n    <div [attr.aria-live]=\"_live\" [attr.role]=\"_role\"></div>\n  </div>\n</div>\n",
                 // In Ivy embedded views will be change detected from their declaration place, rather than
                 // where they were stamped out. This means that we can't have the snack bar container be OnPush,
                 // because it might cause snack bars that were opened from a template not to be out of date.
