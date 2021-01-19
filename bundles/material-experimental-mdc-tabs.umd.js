baa253e006b343c0d1a4609260d69e35e71d8488
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-19 17:42:20 +0000

    11.0.x - 30e351e35 refactor(material/core): clean up deprecation API usages (#21597)

--- bundles/material-experimental-mdc-tabs.umd.js
+++ bundles/material-experimental-mdc-tabs.umd.js
@@ -357,7 +357,7 @@
         { type: core.ChangeDetectorRef }
     ]; };
     MatTabBody.propDecorators = {
-        _portalHost: [{ type: core.ViewChild, args: [portal.PortalHostDirective,] }]
+        _portalHost: [{ type: core.ViewChild, args: [portal.CdkPortalOutlet,] }]
     };
 
     /** Decorates the `ng-template` tags and reads out the template from it. */
