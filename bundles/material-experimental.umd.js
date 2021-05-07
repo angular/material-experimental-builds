c7a1739c66d2ec7993204d39bc446068eb83c52f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-07 05:34:17 +0000

    11.2.x - ea84e7d69 fix(cdk/testing): incorrectly handling ancestor of compound selector (#22476)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('11.2.12-sha-960093992');
+	var VERSION = new core.Version('11.2.12-sha-ea84e7d69');
 
 	/**
 	 * @license
