f3af140aba7369a67e24113f128713bf9169ad02
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-05-13 16:51:04 +0000

    12.0.x - 06c8e7903 fix(material/table): Make table data source interface generic (#22673)

--- mdc-table/public-api.d.ts
+++ mdc-table/public-api.d.ts
@@ -11,3 +11,4 @@ export * from './cell';
 export * from './row';
 export * from './table-data-source';
 export * from './text-column';
+export { MatTableDataSourcePageEvent, MatTableDataSourcePaginator } from '@angular/material/table';
