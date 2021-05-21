cbd74e58c078c13ab580fb697d9e6799e63b989a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-21 12:32:44 +0000

    master - 2f40a8dcc fix(material/slider): incorrectly attributing touches for multiple touch events (#22615)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-62be7b39c');
+	var VERSION = new core.Version('12.1.0-next.0-sha-2f40a8dcc');
 
 	/**
 	 * @license
