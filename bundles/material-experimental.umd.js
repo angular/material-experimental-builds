feeadc7e774ea42eedfc4fc20e0e8b7b010ef1bb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-01 19:50:13 +0000

    master - 21b81d60c docs: explicitly set form field appearance (#22837)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-ade090104');
+	var VERSION = new core.Version('12.1.0-next.0-sha-21b81d60c');
 
 	/**
 	 * @license
