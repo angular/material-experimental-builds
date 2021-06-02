6b868599882c14cca7a4064e4c6eaa51caed838b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-02 23:03:48 +0000

    master - 5ad5c5759 refactor(material/form-field): fix incorrectly named mixin base constant (#22836)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('12.1.0-next.0-sha-e2633d102');
+	var VERSION = new core.Version('12.1.0-next.0-sha-5ad5c5759');
 
 	/**
 	 * @license
