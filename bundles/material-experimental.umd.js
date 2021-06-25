4c4d746bec6abbd2e6310645f39d4d808de701a8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-25 21:32:36 +0000

    master - 8a28f1559 build: remove unnecessary `env.sh` logic for circleci (#23054)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-2458d309b');
+	var VERSION = new core.Version('12.2.0-next.0-sha-8a28f1559');
 
 	/**
 	 * @license
