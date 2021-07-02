79346b8da13e418e9ba1ce569c2d0d720915ea1f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-02 18:47:34 +0000

    master - a18d26f32 fix(cdk/a11y): fake mousedown detection not working (#23029)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-1afbdbda6');
+	var VERSION = new core.Version('12.2.0-next.0-sha-a18d26f32');
 
 	/**
 	 * @license
