c4f941999192380c29b53eb8bdf218a2b8bcd220
Wagner Maciel <wagnermaciel@google.com>
2021-07-21 20:22:31 +0000

    12.1.x - f7a4a1c03 Revert "fix(material/table): switch back to class-based styling for flex tables (#22350)" (#23212)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('12.1.2-sha-881b0137c');
+	var VERSION = new core.Version('12.1.2-sha-f7a4a1c03');
 
 	/**
 	 * @license
