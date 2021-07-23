425562433d91363bdb1225614b716f362e7055e2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-07-23 20:13:52 +0000

    master - 8a1b6f297 build: fix being unable to run e2e tests locally on windows (#23219)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('12.2.0-next.0-sha-c7baa54a9');
+	var VERSION = new core.Version('12.2.0-next.0-sha-8a1b6f297');
 
 	/**
 	 * @license
