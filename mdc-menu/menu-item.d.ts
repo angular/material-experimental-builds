cfa75753f04fe04e10b0473673b7c4a0840279e9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-27 04:50:20 +0000

    12.1.x - fd5f3da52 fix(material-experimental/mdc-list): fix selected indication in high contrast mode (#23242)

--- mdc-menu/menu-item.d.ts
+++ mdc-menu/menu-item.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { ElementRef } from '@angular/core';
+import { ElementRef, ChangeDetectorRef } from '@angular/core';
 import { RippleAnimationConfig, RippleGlobalOptions } from '@angular/material-experimental/mdc-core';
 import { MatMenuItem as BaseMatMenuItem, MatMenuPanel } from '@angular/material/menu';
 import { FocusMonitor } from '@angular/cdk/a11y';
@@ -15,5 +15,5 @@ import { FocusMonitor } from '@angular/cdk/a11y';
 export declare class MatMenuItem extends BaseMatMenuItem {
     _rippleAnimation: RippleAnimationConfig;
     _noopAnimations: boolean;
-    constructor(elementRef: ElementRef<HTMLElement>, document?: any, focusMonitor?: FocusMonitor, parentMenu?: MatMenuPanel<unknown>, globalRippleOptions?: RippleGlobalOptions, animationMode?: string);
+    constructor(elementRef: ElementRef<HTMLElement>, document?: any, focusMonitor?: FocusMonitor, parentMenu?: MatMenuPanel<unknown>, globalRippleOptions?: RippleGlobalOptions, animationMode?: string, changeDetectorRef?: ChangeDetectorRef);
 }
