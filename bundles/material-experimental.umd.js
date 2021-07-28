d393447309a9ef3df35ba048ceea044f78877d86
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-28 08:00:39 +0000

    12.1.x - 23ec0b411 build: install MDC canary dependencies through yarn (#23251)

--- bundles/material-experimental.umd.js
+++ bundles/material-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the Material experimental package. */
-	var VERSION = new core.Version('12.1.3-sha-cdd6663cc');
+	var VERSION = new core.Version('12.1.3-sha-23ec0b411');
 
 	/**
 	 * @license
