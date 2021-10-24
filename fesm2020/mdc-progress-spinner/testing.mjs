44f3fa4ed38b342ab6a8a82a48fa644fb8d48c6c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-10-24 22:39:08 +0000

    13.0.x - 6fa3c1ddd refactor(multiple): clean up explicit teardown flags (#23814)

--- fesm2020/mdc-progress-spinner/testing.mjs
+++ fesm2020/mdc-progress-spinner/testing.mjs
@@ -43,6 +43,14 @@ MatProgressSpinnerHarness.hostSelector = '.mat-mdc-progress-spinner';
  */
 
 /**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
  * Generated bundle index. Do not edit.
  */
 
