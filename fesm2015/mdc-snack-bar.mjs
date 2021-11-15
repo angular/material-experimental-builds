90d02421d0e903a645413527103670dfb0b8757d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-11-15 19:34:15 +0000

    master - 4be9a6e4a refactor(multiple): clean up more ViewEngine-specific code (#23960)

--- fesm2015/mdc-snack-bar.mjs
+++ fesm2015/mdc-snack-bar.mjs
@@ -328,7 +328,6 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.0.1", ngImpor
                         MatSnackBarActions,
                         MatSnackBarAction,
                     ],
-                    entryComponents: [MatSimpleSnackBar, MatSnackBarContainer],
                 }]
         }] });
 
--- selection/row-selection.d.ts
+++ selection/row-selection.d.ts
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 import { CdkRowSelection } from '@angular/cdk-experimental/selection';
 import * as i0 from "@angular/core";
 /**
@@ -10,9 +17,6 @@ import * as i0 from "@angular/core";
 export declare class MatRowSelection<T> extends CdkRowSelection<T> {
     /** The value that is associated with the row */
     value: T;
-    /** The index of the value in the list. Required when used with `trackBy` */
-    get index(): number | undefined;
-    set index(index: number | undefined);
     static ɵfac: i0.ɵɵFactoryDeclaration<MatRowSelection<any>, never>;
-    static ɵdir: i0.ɵɵDirectiveDeclaration<MatRowSelection<any>, "[matRowSelection]", never, { "value": "matRowSelectionValue"; "index": "matRowSelectionIndex"; }, {}, never>;
+    static ɵdir: i0.ɵɵDirectiveDeclaration<MatRowSelection<any>, "[matRowSelection]", never, { "index": "matRowSelectionIndex"; "value": "matRowSelectionValue"; }, {}, never>;
 }
