df6c434d74c7d1064519335c12721a92aee14200
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2022-01-13 20:57:16 +0000

    master - ffc1de4f7 fix(material-experimental/mdc-snack-bar): avoid hard reference to base components and align API (#21425)

--- mdc-snack-bar/module.d.ts
+++ mdc-snack-bar/module.d.ts
@@ -9,6 +9,6 @@ import * as i7 from "@angular/material-experimental/mdc-button";
 import * as i8 from "@angular/material-experimental/mdc-core";
 export declare class MatSnackBarModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<MatSnackBarModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<MatSnackBarModule, [typeof i1.MatSimpleSnackBar, typeof i2.MatSnackBarContainer, typeof i3.MatSnackBarLabel, typeof i3.MatSnackBarActions, typeof i3.MatSnackBarAction], [typeof i4.OverlayModule, typeof i5.PortalModule, typeof i6.CommonModule, typeof i7.MatButtonModule, typeof i8.MatCommonModule], [typeof i8.MatCommonModule, typeof i2.MatSnackBarContainer, typeof i3.MatSnackBarLabel, typeof i3.MatSnackBarActions, typeof i3.MatSnackBarAction]>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<MatSnackBarModule, [typeof i1.SimpleSnackBar, typeof i2.MatSnackBarContainer, typeof i3.MatSnackBarLabel, typeof i3.MatSnackBarActions, typeof i3.MatSnackBarAction], [typeof i4.OverlayModule, typeof i5.PortalModule, typeof i6.CommonModule, typeof i7.MatButtonModule, typeof i8.MatCommonModule], [typeof i8.MatCommonModule, typeof i2.MatSnackBarContainer, typeof i3.MatSnackBarLabel, typeof i3.MatSnackBarActions, typeof i3.MatSnackBarAction]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<MatSnackBarModule>;
 }
--- mdc-snack-bar/snack-bar.d.ts
+++ mdc-snack-bar/snack-bar.d.ts
@@ -1,14 +1,26 @@
-import { MatSnackBar as BaseMatSnackBar } from '@angular/material/snack-bar';
-import { MatSimpleSnackBar } from './simple-snack-bar';
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Overlay } from '@angular/cdk/overlay';
+import { Injector } from '@angular/core';
+import { MatSnackBarConfig, _MatSnackBarBase } from '@angular/material/snack-bar';
+import { SimpleSnackBar } from './simple-snack-bar';
 import { MatSnackBarContainer } from './snack-bar-container';
 import * as i0 from "@angular/core";
 /**
  * Service to dispatch Material Design snack bar messages.
  */
-export declare class MatSnackBar extends BaseMatSnackBar {
-    protected simpleSnackBarComponent: typeof MatSimpleSnackBar;
+export declare class MatSnackBar extends _MatSnackBarBase {
+    protected simpleSnackBarComponent: typeof SimpleSnackBar;
     protected snackBarContainerComponent: typeof MatSnackBarContainer;
     protected handsetCssClass: string;
-    static ɵfac: i0.ɵɵFactoryDeclaration<MatSnackBar, never>;
+    constructor(overlay: Overlay, live: LiveAnnouncer, injector: Injector, breakpointObserver: BreakpointObserver, parentSnackBar: MatSnackBar, defaultConfig: MatSnackBarConfig);
+    static ɵfac: i0.ɵɵFactoryDeclaration<MatSnackBar, [null, null, null, null, { optional: true; skipSelf: true; }, null]>;
     static ɵprov: i0.ɵɵInjectableDeclaration<MatSnackBar>;
 }
