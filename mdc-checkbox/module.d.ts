32a760410b3eb8c40f09db5d4ccd7b98cc6dc1af
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2022-01-25 16:27:58 +0000

    13.2.x - 1ce3e5e90 perf(material-experimental/mdc-checkbox): reduce bundle size (#24256)

--- mdc-checkbox/module.d.ts
+++ mdc-checkbox/module.d.ts
@@ -1,10 +1,9 @@
 import * as i0 from "@angular/core";
 import * as i1 from "./checkbox";
 import * as i2 from "@angular/material-experimental/mdc-core";
-import * as i3 from "@angular/common";
-import * as i4 from "@angular/material/checkbox";
+import * as i3 from "@angular/material/checkbox";
 export declare class MatCheckboxModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<MatCheckboxModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<MatCheckboxModule, [typeof i1.MatCheckbox], [typeof i2.MatCommonModule, typeof i2.MatRippleModule, typeof i3.CommonModule, typeof i4._MatCheckboxRequiredValidatorModule], [typeof i1.MatCheckbox, typeof i2.MatCommonModule, typeof i4._MatCheckboxRequiredValidatorModule]>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<MatCheckboxModule, [typeof i1.MatCheckbox], [typeof i2.MatCommonModule, typeof i2.MatRippleModule, typeof i3._MatCheckboxRequiredValidatorModule], [typeof i1.MatCheckbox, typeof i2.MatCommonModule, typeof i3._MatCheckboxRequiredValidatorModule]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<MatCheckboxModule>;
 }
