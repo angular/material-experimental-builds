ae1ca3f989bdbef04acdd590150053e617e03c3f
Zach Arend <zarend@users.noreply.github.com>
2022-02-15 17:26:52 +0000

    13.2.x - 601ab25cb refactor(material/datepicker): unit test with not the first of the month (#24401)

--- mdc-sidenav/module.d.ts
+++
@@ -1,7 +0,0 @@
-import * as i0 from "@angular/core";
-import * as i1 from "@angular/material-experimental/mdc-core";
-export declare class MatSidenavModule {
-    static ɵfac: i0.ɵɵFactoryDeclaration<MatSidenavModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<MatSidenavModule, never, [typeof i1.MatCommonModule], [typeof i1.MatCommonModule]>;
-    static ɵinj: i0.ɵɵInjectorDeclaration<MatSidenavModule>;
-}
