e488a9362bc1be750f1b1f75995898601d4b3a96
Andrew Seguin <andrewseguin@users.noreply.github.com>
2022-03-05 23:30:37 +0000

    13.2.x - 6e1d1c0e0 test: provide a test component that opens components in a dialog (#24522)

--- mdc-dialog/testing/public-api.d.ts
+++ mdc-dialog/testing/public-api.d.ts
@@ -7,3 +7,4 @@
  */
 export { DialogHarnessFilters } from '@angular/material/dialog/testing';
 export { MatDialogHarness, MatDialogSection } from './dialog-harness';
+export * from './dialog-opener';
