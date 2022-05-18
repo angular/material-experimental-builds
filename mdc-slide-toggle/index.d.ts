52d44ae54f6620d217a6048ab283919f99a502e0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2022-05-18 11:57:17 +0000

    14.0.x - c710b1408 refactor(material-experimental/mdc-slide-toggle): remove usage of MDC adapter (#24935)

--- mdc-slide-toggle/index.d.ts
+++ mdc-slide-toggle/index.d.ts
@@ -1,9 +1,6 @@
-import { AfterViewInit } from '@angular/core';
-import { BooleanInput } from '@angular/cdk/coercion';
 import { ChangeDetectorRef } from '@angular/core';
 import { ControlValueAccessor } from '@angular/forms';
 import { ElementRef } from '@angular/core';
-import { EventEmitter } from '@angular/core';
 import { FocusMonitor } from '@angular/cdk/a11y';
 import * as i0 from '@angular/core';
 import * as i2 from '@angular/material/slide-toggle';
@@ -11,10 +8,10 @@ import * as i3 from '@angular/material-experimental/mdc-core';
 import * as i4 from '@angular/common';
 import { InjectionToken } from '@angular/core';
 import { MAT_SLIDE_TOGGLE_REQUIRED_VALIDATOR } from '@angular/material/slide-toggle';
+import { _MatSlideToggleBase } from '@angular/material/slide-toggle';
 import { MatSlideToggleRequiredValidator } from '@angular/material/slide-toggle';
-import { NumberInput } from '@angular/cdk/coercion';
-import { OnDestroy } from '@angular/core';
 import { ThemePalette } from '@angular/material-experimental/mdc-core';
+import { Type } from '@angular/core';
 
 declare namespace i1 {
     export {
@@ -30,86 +27,28 @@ export declare const MAT_SLIDE_TOGGLE_DEFAULT_OPTIONS: InjectionToken<MatSlideTo
 export { MAT_SLIDE_TOGGLE_REQUIRED_VALIDATOR }
 
 /** @docs-private */
-export declare const MAT_SLIDE_TOGGLE_VALUE_ACCESSOR: any;
+export declare const MAT_SLIDE_TOGGLE_VALUE_ACCESSOR: {
+    provide: InjectionToken<readonly ControlValueAccessor[]>;
+    useExisting: Type<any>;
+    multi: boolean;
+};
 
-export declare class MatSlideToggle implements ControlValueAccessor, AfterViewInit, OnDestroy {
-    private _elementRef;
-    private _focusMonitor;
-    private _changeDetectorRef;
-    defaults: MatSlideToggleDefaultOptions;
-    private _onChange;
-    private _onTouched;
-    private _uniqueId;
-    private _required;
-    private _checked;
-    private _foundation;
-    private _adapter;
-    /** Whether the slide toggle is currently focused. */
-    _focused: boolean;
-    /** Whether noop animations are enabled. */
-    _noopAnimations: boolean;
+export declare class MatSlideToggle extends _MatSlideToggleBase<MatSlideToggleChange> {
     /** Unique ID for the label element. */
     _labelId: string;
-    /** The color palette  for this slide toggle. */
-    color: ThemePalette;
-    /** Name value will be applied to the button element if present. */
-    name: string | null;
-    /** A unique id for the slide-toggle button. If none is supplied, it will be auto-generated. */
-    id: string;
-    /** Tabindex for the input element. */
-    get tabIndex(): number;
-    set tabIndex(value: NumberInput);
-    private _tabIndex;
-    /** Whether the label should appear after or before the slide-toggle. Defaults to 'after'. */
-    labelPosition: 'before' | 'after';
-    /** Used to set the aria-label attribute on the underlying button element. */
-    ariaLabel: string | null;
-    /** Used to set the aria-labelledby attribute on the underlying button element. */
-    ariaLabelledby: string | null;
-    /** Used to set the aria-describedby attribute on the underlying button element. */
-    ariaDescribedby: string;
-    /** Whether the slide-toggle is required. */
-    get required(): boolean;
-    set required(value: BooleanInput);
-    /** Whether the slide-toggle element is checked or not. */
-    get checked(): boolean;
-    set checked(value: BooleanInput);
-    /** Whether to disable the ripple on this checkbox. */
-    get disableRipple(): boolean;
-    set disableRipple(disableRipple: BooleanInput);
-    private _disableRipple;
-    /** Whether the slide toggle is disabled. */
-    get disabled(): boolean;
-    set disabled(disabled: BooleanInput);
-    private _disabled;
-    /** An event will be dispatched each time the slide-toggle changes its value. */
-    readonly change: EventEmitter<MatSlideToggleChange>;
-    /** Event will be dispatched each time the slide-toggle input is toggled. */
-    readonly toggleChange: EventEmitter<void>;
     /** Returns the unique id for the visual hidden button. */
     get buttonId(): string;
     /** Reference to the MDC switch element. */
     _switchElement: ElementRef<HTMLElement>;
-    constructor(_elementRef: ElementRef, _focusMonitor: FocusMonitor, _changeDetectorRef: ChangeDetectorRef, tabIndex: string, defaults: MatSlideToggleDefaultOptions, animationMode?: string);
-    ngAfterViewInit(): void;
-    ngOnDestroy(): void;
+    constructor(elementRef: ElementRef, focusMonitor: FocusMonitor, changeDetectorRef: ChangeDetectorRef, tabIndex: string, defaults: MatSlideToggleDefaultOptions, animationMode?: string);
     /** Method being called whenever the underlying button is clicked. */
-    _handleClick(event: Event): void;
-    /** Implemented as part of ControlValueAccessor. */
-    writeValue(value: any): void;
-    /** Implemented as part of ControlValueAccessor. */
-    registerOnChange(fn: any): void;
-    /** Implemented as part of ControlValueAccessor. */
-    registerOnTouched(fn: any): void;
-    /** Implemented as a part of ControlValueAccessor. */
-    setDisabledState(isDisabled: boolean): void;
+    _handleClick(): void;
     /** Focuses the slide-toggle. */
     focus(): void;
-    /** Toggles the checked state of the slide-toggle. */
-    toggle(): void;
+    protected _createChangeEvent(isChecked: boolean): MatSlideToggleChange;
     _getAriaLabelledBy(): string | null;
     static ɵfac: i0.ɵɵFactoryDeclaration<MatSlideToggle, [null, null, null, { attribute: "tabindex"; }, null, { optional: true; }]>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<MatSlideToggle, "mat-slide-toggle", ["matSlideToggle"], { "color": "color"; "name": "name"; "id": "id"; "tabIndex": "tabIndex"; "labelPosition": "labelPosition"; "ariaLabel": "aria-label"; "ariaLabelledby": "aria-labelledby"; "ariaDescribedby": "aria-describedby"; "required": "required"; "checked": "checked"; "disableRipple": "disableRipple"; "disabled": "disabled"; }, { "change": "change"; "toggleChange": "toggleChange"; }, never, ["*"], false>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<MatSlideToggle, "mat-slide-toggle", ["matSlideToggle"], { "disabled": "disabled"; "disableRipple": "disableRipple"; "color": "color"; "tabIndex": "tabIndex"; }, {}, never, ["*"], false>;
 }
 
 /** Change event object emitted by a MatSlideToggle. */
