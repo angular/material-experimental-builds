8b7916bc6563e966872d7b7fed728b50b6259301
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2023-04-06 19:15:29 +0000

    16.0.x - 7d9aac9de release: cut the v16.0.0-next.5 release

--- fesm2020/material-experimental.mjs
+++ fesm2020/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('16.0.0-next.4');
+const VERSION = new Version('16.0.0-next.5');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
