f136def7275074226966ef6b26be2ed5bf89f6b8
Wagner Maciel <wagnermaciel@google.com>
2023-04-19 22:22:26 +0000

    16.0.x - a69da91a0 release: cut the v16.0.0-rc.1 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('16.0.0-rc.0');
+const VERSION = new Version('16.0.0-rc.1');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
