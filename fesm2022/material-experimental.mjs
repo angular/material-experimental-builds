f00c4f071416f8e3b4eec53006307507810c433f
Zach Arend <zachzach@google.com>
2023-08-16 21:18:21 +0000

    16.2.x - ebe36eb19 release: cut the v16.2.1 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('16.2.0');
+const VERSION = new Version('16.2.1');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
