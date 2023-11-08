9a13ece229717d5a0202c2a904ee5d12d1ca24d6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2023-11-08 12:59:54 +0000

    main - beb564c build: update to Angular v17 final (#28076)

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('17.1.0-next.0');
+const VERSION = new Version('17.0.0-rc.3');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
