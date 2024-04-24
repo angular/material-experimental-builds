74dade74167b8a99c5f4cfec6827c902a304e8ca
Miles Malerba <mmalerba@google.com>
2024-04-24 21:28:46 +0000

    17.3.x - 5cdf38b release: cut the v17.3.6 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('17.3.5');
+const VERSION = new Version('17.3.6');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
