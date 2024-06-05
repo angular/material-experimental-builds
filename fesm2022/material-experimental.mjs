6f24f48d4581b5d4a83469a8b2ea87d818ddf59e
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2024-06-05 19:53:37 +0000

    main - 03e10df release: cut the v18.1.0-next.1 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('18.1.0-next.0');
+const VERSION = new Version('18.1.0-next.1');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
