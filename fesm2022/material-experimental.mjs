0f32cd0809a7aa82ee99c16a1638e073d4eac824
Andrew Seguin <andrewjs@google.com>
2025-03-05 20:45:51 +0000

    19.2.x - 9061b7b release: cut the v19.2.2 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('19.2.1');
+const VERSION = new Version('19.2.2');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
