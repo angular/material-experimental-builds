f7e504f1872c02167cc808f0fc76bcd5532d3bb1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-20 17:20:52 +0000

    19.2.x - 2a3c254 release: cut the v19.2.5 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('19.2.4');
+const VERSION = new Version('19.2.5');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
