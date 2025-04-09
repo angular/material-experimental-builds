677388082cc6378279ceb54526ea7c6b2adaf149
Wagner Maciel <wagnermaciel@google.com>
2025-04-09 18:10:48 +0000

    19.2.x - 94c2c0a release: cut the v19.2.9 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('19.2.8');
+const VERSION = new Version('19.2.9');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
