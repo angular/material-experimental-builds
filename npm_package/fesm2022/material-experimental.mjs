b7242533ff9263ec91ab9c6913665e33aa8688de
Joey Perrott <josephperrott@gmail.com>
2025-04-23 18:20:32 +0000

    main - fba42e3  Migrate to use rules_js based ng_package (#30935)

---
+++ npm_package/fesm2022/material-experimental.mjs
@@ -0,0 +1,7 @@
+import { Version } from '@angular/core';
+
+/** Current version of the Material experimental package. */
+const VERSION = new Version('20.0.0-next.8');
+
+export { VERSION };
+//# sourceMappingURL=material-experimental.mjs.map
---
+++ npm_package/index.d.ts
@@ -0,0 +1,6 @@
+import { Version } from '@angular/core';
+
+/** Current version of the Material experimental package. */
+declare const VERSION: Version;
+
+export { VERSION };
