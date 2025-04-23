ebe71ee9b3f0f71065e776be256df851d43836a5
Miles Malerba <mmalerba@google.com>
2025-04-23 16:54:24 +0000

    19.2.x - 7b1d658 release: cut the v19.2.11 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('19.2.10');
+const VERSION = new Version('19.2.11');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
