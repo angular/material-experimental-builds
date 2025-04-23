b2272ca2fb024dc5560b84da0143044c94c19e17
Joey Perrott <josephperrott@gmail.com>
2025-04-23 18:20:40 +0000

    19.2.x - 54cc2cf [PATCH] Migrate to use rules_js based ng_package (#30940)

---
+++ npm_package/fesm2022/material-experimental.mjs
@@ -0,0 +1,7 @@
+import { Version } from '@angular/core';
+
+/** Current version of the Material experimental package. */
+const VERSION = new Version('19.2.11');
+
+export { VERSION };
+//# sourceMappingURL=material-experimental.mjs.map
