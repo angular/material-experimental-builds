99afe0f6602c9f9a104771cbed79d02ff51650e9
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-01 12:27:57 +0000

    19.2.x - 91ea605 release: cut the v19.2.14 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('19.2.13');
+const VERSION = new Version('19.2.14');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
