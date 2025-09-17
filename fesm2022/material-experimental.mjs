0e5802e27e5e57885d6041626ea05113efb5cd1d
Cheng-Hsuan Tsai <webcat@google.com>
2025-09-17 18:46:46 +0000

    main - a6fbdd9 release: cut the v21.0.0-next.4 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('21.0.0-next.3');
+const VERSION = new Version('21.0.0-next.4');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
