dd27c92d092cee100951c3adfabae664c760a58a
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2025-09-24 20:47:47 +0000

    20.2.x - 50caec7 release: cut the v20.2.5 release

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the Material experimental package. */
-const VERSION = new Version('20.2.4');
+const VERSION = new Version('20.2.5');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
