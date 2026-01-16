f832efaa25c460d7ccb2ba99c1dfcd5682f1058d
Angular Robot <angular-robot@google.com>
2026-01-16 10:38:42 +0000

    21.1.x - 6c61db8 build: update dependency node to v22.22.0 (#32660)

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0+sha-f9b3695');
+const VERSION = new Version('21.1.0+sha-6c61db8');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
