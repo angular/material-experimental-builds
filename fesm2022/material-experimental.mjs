b3ad54f6112bcc5fd86d086a42be3d099ffaf84b
Angular Robot <angular-robot@google.com>
2026-08-21 09:32:06 +0000

    main - b91d6da build: update pnpm to v11.22.0 (#33687)

--- fesm2022/material-experimental.mjs
+++ fesm2022/material-experimental.mjs
@@ -1,6 +1,6 @@
 import { Version } from '@angular/core';
 
-const VERSION = new Version('22.2.0-next.2+sha-5cccb18-with-local-changes');
+const VERSION = new Version('22.2.0-next.2+sha-b91d6da');
 
 export { VERSION };
 //# sourceMappingURL=material-experimental.mjs.map
